2002aa529489f3a1c1a58c2ba82c073ac0a40179
sotoc <sotoc@COMANE173>
2016-08-30 16:57:50 -0500

    Constante

---
+++ iFlota.Forms/iFlota.Forms/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace iFlota.Forms
+{
+    /// <summary>
+    /// Clase para el manejo de constantes de la aplicación
+    /// </summary>
+    public static class Constants
+    {
+        //URL para las consultas de azure
+        public static string ApplicationURL = @"https://iflota.azurewebsites.net";
+    }
+}
