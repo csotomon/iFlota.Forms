2566ead0b4d2e98f4916906add4c71d7a19f0e16
Camilo Andres Soto Montoya <csotomon@hotmail.com>
2016-09-10 10:08:53 -0500

    Splash gama de colores azul

---
+++ iFlota.Forms/iFlota.Forms/Statics/PaletaMaterial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace iFlota.Forms.Statics
+{
+	public static class PaletaMaterial
+	{
+		public static readonly Color _P1 = Color.FromHex("303F9F");
+		public static readonly Color _P2 = Color.FromHex("3F51B5");
+		public static readonly Color _P3 = Color.FromHex("C5CAE9");
+		public static readonly Color _A1 = Color.FromHex("F50057");
+		public static readonly Color _A2 = Color.FromHex("FF80AB");
+		public static readonly Color _A3 = Color.FromHex("FF4081");
+	}
+
+}
+
