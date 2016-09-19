26a6d5bac9ab4d8d93b25af9355ed52b8fa1ec5e
Camilo Soto Montoya <csotomon@hotmail.com>
2016-09-18 21:14:28 -0500

    Organizacion de archivos

--- iFlota.Forms/iFlota.Forms.Droid/Servicios/Autenticador.cs
+++ iFlota.Forms/iFlota.Forms.Droid/Servicios/Autenticador.cs
@@ -7,20 +7,27 @@ using Xamarin.Forms;
 [assembly: Dependency(typeof(iFlota.Forms.Droid.Servicios.Autenticador))]
 namespace iFlota.Forms.Droid.Servicios
 {
-	public class Autenticador : IAutenticador
+    /// <summary>
+    /// Clase que implementa la interfaz de autenticador de la libreria portable
+    /// </summary>
+    public class Autenticador : IAutenticador
 	{
-		MobileServiceClient mobileClient;
+        // Cliente del servicio mobil de azure
+        MobileServiceClient mobileClient;
+        // Usuario autenticado del servicio mobil de azure
 		MobileServiceUser mobileUser;
 
 		public Autenticador() {
-			mobileClient = new MobileServiceClient(Constants.ApplicationURL);
+            //Inicializacion de la libreria de azure
+            mobileClient = new MobileServiceClient(Constants.ApplicationURL);
 		}
-
-		public async Task<MobileServiceUser> Autenticar(string autoridad, string recurso, string clienteId, string returnUri)
+        
+		public async Task<MobileServiceUser> Autenticar(string autoridad)
 		{
 			try
 			{
-				var resultado= await mobileClient.LoginAsync(Xamarin.Forms.Forms.Context, autoridad);
+                // Llama a la interfaz de loggueo de azure, de acuerdo a la autoridad escogida
+                var resultado= await mobileClient.LoginAsync(Xamarin.Forms.Forms.Context, autoridad);
 				return resultado;
 			}
 			catch (Exception ex)
@@ -31,12 +38,14 @@ namespace iFlota.Forms.Droid.Servicios
 
 		public Task<bool> DesAutenticar(string autoridad)
 		{
-			throw new NotImplementedException();
+            // TODO: Falta realizar
+            throw new NotImplementedException();
 		}
 
 		public Task<string> ExtraerToken(string autoridad)
 		{
-			throw new NotImplementedException();
+            // TODO: Falta realizar
+            throw new NotImplementedException();
 		}
 	}
 }
